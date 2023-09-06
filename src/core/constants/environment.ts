b33a4d9a714bb95afb5c38c85dbc1dc085b74c18
pedrolauro15 <pedrolauro15@gmail.com>
2023-09-06 20:19:39 -0300

    Fist commit

---
+++ src/core/constants/environment.ts
@@ -0,0 +1,2 @@
+export const APP_PORT = process.env.PORT || 44044;
+export const APP_ORIGIN = process.env.APP_ORIGIN;
---
+++ src/main.ts
@@ -0,0 +1,13 @@
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { APP_ORIGIN, APP_PORT } from "./core/constants/environment";
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(APP_PORT);
+
+  app.enableCors({
+    origin: APP_ORIGIN,
+  });
+}
+bootstrap();
